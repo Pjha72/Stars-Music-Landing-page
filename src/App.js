bfe75d74e30dc9a0623658532a27877ea0cd69df
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-05-06 12:57:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-
+import React from 'react'
 import Download from './components/Download';
 import Experince from './components/Experince';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Search from './components/Search';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById('spinner');
+  if(spinner){
+    setTimeout(()=>{
+      spinner.style.display="none";
+      setLoading(false);
+    },3000)
+  }
   return (
-    
-    <div className='App text-white overflow-hidden'>
+    !loading && (
+      <div className='App text-white overflow-hidden'>
       <Header/>
       <Hero/>
       <Experince/>
@@ -17,6 +26,7 @@ function App() {
       <Download/>
       <Footer/>
     </div>
+    )
   );
 }
 
